30a9cbf999cb5112eee039bea8f1529068d31d3c
Alef Silva <alef.dev.silva@gmail.com>
2022-07-11 20:58:47 -0300

    Adicionar arquivos de projeto.

---
+++ Infra/Entidade/ContextBase.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infra.Entidade
+{
+    public class ContextBase : DbContext
+    {
+        public ContextBase(DbContextOptions<ContextBase> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+        public DbSet<SolicitacaoEmail> SolicitacaoEmail { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+                optionsBuilder.UseSqlServer(GetStringConnectionConfig());
+        }
+        private string GetStringConnectionConfig()
+        {
+            string strCon = "Server=localhost\\SQLEXPRESS;Database=FilaEmail;Trusted_Connection=True;";
+            return strCon;
+        }
+    }
+}
---
+++ Infra/Entidade/SolicitacaoEmail.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Infra.Entidade
+{
+    [Table("SolicitacaoEmail")]
+    public class SolicitacaoEmail
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+        [Column("Titulo")]
+        public string Titulo { get; set; }
+        [Column("Mensagem")]
+        public string Mensagem { get; set; }
+        [Column("Destinatarios")]
+        public string Destinatarios { get; set; }
+        [Column("Enviado")]
+        public bool Enviado { get; set; }
+    }
+}
